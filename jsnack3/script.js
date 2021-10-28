d9719a30afe1f8581be04de0ef5f3aa226035c22
Jacopofede89 <jacopofede89@gmail.com>
2021-10-28 21:22:23 +0200

    #3 ex finale

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,15 @@
 // // il sosoftware deve chiedere per 10 volte di inserire un numero
 // // il programma stampa la somma dei numeri
 
+
+let numbers = []
+let sum = 0;
+
+for(let i = 0; i < 10; i++){
+    numbers.push(parseInt(prompt("inserisci un numero: ")));
+    sum += numbers[i];
+    
+}
+document.getElementById("result").innerHTML = `il risultato è: ${sum}`;
+console.log("il risultato è: ", sum);
+
